e038d9fa1be005fd0407f634ec86021df84be461
Athera <@7>
2024-05-09 20:36:41 +0300

    Me when uhhh when thee when 1 little happy star running around He is a happy guy, he is running now Wh 2 little happy stars running around They are running because they’re happy star 3 little happy stars running around This is a nursery rhyme about Happy Star 4 little- happy stars running around They like to run sometimes- yeah 5 little happy stars running around Running in circles all around 6 little happy stars running around They are running around 7 little happy stars running around They like to run around because they run 5- little- happy stars running around They like- they like to run, and have fun 7

---
+++ src/main/java/net/ayetherah/bams/BricksAndMetalSticksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ayetherah.bams;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BricksAndMetalSticksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
