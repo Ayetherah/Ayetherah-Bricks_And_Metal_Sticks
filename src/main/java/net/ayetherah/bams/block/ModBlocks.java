13588fe84fb65de8f0e037868353953edd98e373
Athera <@7>
2024-05-16 15:10:14 +0300

    Experimental features

--- src/main/java/net/ayetherah/bams/block/ModBlocks.java
+++ src/main/java/net/ayetherah/bams/block/ModBlocks.java
@@ -16,6 +16,53 @@ public class ModBlocks {
     public static final Block FULL_GRATE = registerBlock("full_grate",
             new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(ModSounds.GRATE_SOUNDS)));
 
+    public static final Block BRICKS_TILE = registerBlock("bricks_tile",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_THAT_IS_NOT_HOW_BRICKS_WORK = registerBlock("bricks_that_is_not_how_bricks_work",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_TALL_WINDOW_BOTTOM = registerBlock("bricks_tall_window_bottom",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_TALL_WINDOW_MIDDLE = registerBlock("bricks_tall_window_middle",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_TALL_WINDOW_TOP = registerBlock("bricks_tall_window_top",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //experimental feature blocks
+
+    public static final Block BRICKS_EXPANDED = registerBlock("bricks_expanded",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_EXPANDED_RTT = registerBlock("bricks_expanded_rtt",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_EXPANDED_C1 = registerBlock("bricks_expanded_c1",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_EXPANDED_C2 = registerBlock("bricks_expanded_c2",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_EXPANDED_C3 = registerBlock("bricks_expanded_c3",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
+    public static final Block BRICKS_EXPANDED_C4 = registerBlock("bricks_expanded_c4",
+            new Block(FabricBlockSettings.copyOf(Blocks.BRICKS)));
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
--- src/main/java/net/ayetherah/bams/item/ModItemGroups.java
+++ src/main/java/net/ayetherah/bams/item/ModItemGroups.java
@@ -17,6 +17,17 @@ public class ModItemGroups {
                     .icon(() -> new ItemStack(ModItems.DROOP)).entries((displayContext, entries) -> {
                         entries.add(ModItems.DROOP);
                         entries.add(ModBlocks.FULL_GRATE);
+                        entries.add(ModBlocks.BRICKS_TILE);
+                        entries.add(ModBlocks.BRICKS_THAT_IS_NOT_HOW_BRICKS_WORK);
+                        entries.add(ModBlocks.BRICKS_TALL_WINDOW_BOTTOM);
+                        entries.add(ModBlocks.BRICKS_TALL_WINDOW_MIDDLE);
+                        entries.add(ModBlocks.BRICKS_TALL_WINDOW_TOP);
+                        entries.add(ModBlocks.BRICKS_EXPANDED);
+                        entries.add(ModBlocks.BRICKS_EXPANDED_RTT);
+                        entries.add(ModBlocks.BRICKS_EXPANDED_C1);
+                        entries.add(ModBlocks.BRICKS_EXPANDED_C2);
+                        entries.add(ModBlocks.BRICKS_EXPANDED_C3);
+                        entries.add(ModBlocks.BRICKS_EXPANDED_C4);
                     }).build());
 
     public static void registerItemGroups() {
